926a5a5868756a71aec137d1c355f781a486594f
Monalika-tech <monalikagupta1336@gmail.com>
2026-01-06 11:16:38 +0530

    bcrypt password andgenerate token

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -2,10 +2,16 @@ import { createContext, useState, useEffect } from "react";
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  const [teacher, setTeacher] = useState(null);
+  const [teacher, setTeacher] = useState(() => {
+    const storedTeacher = localStorage.getItem("teacher");
+    return storedTeacher ? JSON.parse(storedTeacher) : null;
+  });
 
   const loginTeacher = async (email, password) => {
     try {
+      console.log("Logging in with:", { email, password });
+
+      
       const res = await fetch("http://localhost:1337/api/teachers/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -14,12 +20,14 @@ export const AuthContextProvider = ({ children }) => {
 
       console.log(" res we get from backend : ", res);
       const data = await res.json();
+      console.log("Response data:", data.error);
 
       if (!res.ok) {
-        return { success: false, message: data.message };
+        return { success: false, message: data.message , error: data.error};
       }
-
       setTeacher(data.teacher);
+      localStorage.setItem("teacher", JSON.stringify(data.teacher));
+      localStorage.setItem("token", data.token);
       return { success: true };
     } catch (error) {
       return { success: false, message: "Server not reachable" };
@@ -28,6 +36,8 @@ export const AuthContextProvider = ({ children }) => {
 
   const logoutTeacher = () => {
     setTeacher(null);
+    localStorage.removeItem("token");
+    localStorage.removeItem("teacher");
   };
 
   return (
