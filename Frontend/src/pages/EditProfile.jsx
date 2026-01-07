d39c49bf62bcfcfdb75837dd00e6cb454b87e319
Monalika-tech <monalikagupta1336@gmail.com>
2026-01-07 23:14:52 +0530

    usestate added - formdata

--- Frontend/src/pages/EditProfile.jsx
+++ Frontend/src/pages/EditProfile.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import Title from "../components/Title";
 import { AuthContext } from "../context/AuthContext";
 
-
-
-
 const EditProfile = () => {
+  const { teacher, updateTeacher } = useContext(AuthContext);
+  
+
+  const [formData, setFormData] = useState({
+    name: teacher?.name || "",
+    email: teacher?.email || "",
+    phone_no: teacher?.phone_no || "",
+    address: teacher?.address || "",
+    qualification: teacher?.qualification || "",
+    experiencedYears: teacher?.experiencedYears || "",
+  });
   return (
     <div className="sm:w-1/2 flex flex-col items-center justify-center py-10 sm:py-20 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 hover:border-green-500 mx-auto my-20 px-4">
       {/* Title */}
@@ -15,10 +23,8 @@ const EditProfile = () => {
           Need to edit profile!!
         </p>
       </div>
+      <div></div>
     </div>
-
-    // form
-
   );
 };
 
