45a4f2bcdbeec045a8be8695731303e95ecb1b92
Monalika-tech <monalikagupta1336@gmail.com>
2026-02-02 12:59:24 +0530

    Role Authorization

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,19 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-
-import { StudentContextProvider } from "./context/studentContext.jsx";
-import { ClassContextProvider } from "./context/classContext.jsx";
-import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <AuthContextProvider>
-      <ClassContextProvider>
-        <StudentContextProvider>
-          <App />
-        </StudentContextProvider>
-      </ClassContextProvider>
-    </AuthContextProvider>
-  </BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>,
 );
