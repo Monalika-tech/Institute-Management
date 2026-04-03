184afc16bda0b46ad5ab3e9cc9e530862bf07677
Monalika-tech <monalikagupta1336@gmail.com>
2026-04-03 16:48:21 +0530

    add notification schema with type based targeting (general/class)

---
+++ Backend/models/notificationModels.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema(
+    {
+        title: { type: String, required: true },
+        message: { type: String, required: true },
+
+        type: {
+            type: String,
+            enum: ["general", "class"],
+            required: true,
+        },
+
+        classId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Class",
+            default: null,
+        },
+
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Teacher",
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Notification", notificationSchema);
